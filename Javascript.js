907adf268ae2ac2517509c278fcaef7b0754ee4b
Maxime DEFRETIN <maxime.defretin@laposte.net>
2017-10-25 14:34:53 +0200

    Modifs

--- Javascript.js
+++ Javascript.js
@@ -1,6 +1,10 @@
 function form1() {
   var nom = document.getElementById("nom").value;
-var prenom = document.getElementById("prenom").value;
+  var prenom = document.getElementById("prenom").value;
   var ville = document.getElementById("ville").value;
-  alert("Nom : " + nom + '\n' + "Prénom : " + prenom + '\n' + "Ville : " + ville);
+  //isNan() verifie que le paramètre n'est pas un nombre
+  if(nom && ville && prenom && isNaN(nom) == true && isNaN(prenom) == true && isNaN(ville) == true){
+    var result = 'Nom : ' + nom + '\n' + 'Prénom : ' + prenom + '\n' + 'Ville : ' + ville;
+    alert(result);
+  }
 }
